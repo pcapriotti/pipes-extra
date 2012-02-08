ff89d08baabb8d5daaa096f555d68d5fe98110fc
Paolo Capriotti <p.capriotti@gmail.com>
2012-02-08 14:43:54 +0000

    Move everything into a misc directory.

---
+++ misc/Benchmarks/simple.hs
@@ -0,0 +1,23 @@
+import Criterion.Main
+
+import Control.Monad
+import Control.Monad.Trans
+import Control.Pipe
+import System.IO
+
+printer :: Show a => Handle -> Pipe a x IO r
+printer h = forever $ await >>= lift . hPutStrLn h . show
+
+devnull :: IO Handle
+devnull = openFile "/dev/null" WriteMode
+
+main :: IO ()
+main = do
+    h <- devnull
+    defaultMain
+      [ bench "list >+> printer (1000)" (whnfIO $ runPipe $ mapM_ yield [1..1000 :: Int] >+> printer h)
+      , bench "mapM_ print (1000)" (whnfIO $ mapM_ (hPutStrLn h . show) [1..1000 :: Int])
+      , bench "list >+> printer (100000)" (whnfIO $ runPipe $ mapM_ yield [1..100000 :: Int] >+> printer h)
+      , bench "mapM_ print (100000)" (whnfIO $ mapM_ (hPutStrLn h . show) [1..100000 :: Int])
+      ]
+
