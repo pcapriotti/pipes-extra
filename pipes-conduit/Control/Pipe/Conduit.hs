e682b7c21fd30593d83bade58996a67af8fff794
Paolo Capriotti <p.capriotti@gmail.com>
2012-02-20 09:59:38 +0000

    Port to finalizable pipes.

--- pipes-conduit/Control/Pipe/Conduit.hs
+++ pipes-conduit/Control/Pipe/Conduit.hs
@@ -10,7 +10,7 @@ module Control.Pipe.Conduit (
   sinkPipe_
   ) where
 
-import Control.Monad.Trans
+import Control.Monad (void)
 import Control.Monad.Trans.Resource
 import Control.Pipe
 import Data.Conduit
@@ -20,61 +20,51 @@ import Data.Conduit
 -- The resulting pipe behaves like the original 'Conduit', and closes it upon
 -- termination. Any unconsumed input is returned.
 conduitPipe :: Resource m => Conduit a m b -> Pipe a b (ResourceT m) (Maybe a)
-conduitPipe c = do
-  (PreparedConduit push close) <- lift $ prepareConduit c
-  go push close
-  where
-    go push close = do
-      x <- tryAwait
-      case x of
-        Nothing -> lift close >>= mapM_ yield >> return Nothing
-        Just input -> do
-          result <- lift $ push input
-          case result of
-            Producing output -> mapM_ yield output >> go push close
-            Finished input' output -> mapM_ yield output >> return input'
+conduitPipe (Conduit push close) = do
+  x <- tryAwait
+  case x of
+    Nothing -> lift close >>= mapM_ yield >> return Nothing
+    Just input -> do
+      result <- lift $ push input
+      case result of
+        Producing c' output -> mapM_ yield output >> conduitPipe c'
+        Finished input' output -> mapM_ yield output >> return input'
 
 -- | Convert a 'Conduit' to a 'Pipe', ignoring unconsumed input.
 conduitPipe_ :: Resource m => Conduit a m b -> Pipe a b (ResourceT m) ()
-conduitPipe_ c = conduitPipe c >> return ()
+conduitPipe_ = void . conduitPipe
 
 -- | Convert a 'Source' into a 'Pipe'.
 --
 -- The resulting 'Pipe' is a 'Producer' which pulls from the 'Source' until
 -- exhaustion and yields the received data.
 sourcePipe :: Resource m => Source m a -> Pipe x a (ResourceT m) ()
-sourcePipe s = do
-  (PreparedSource pull close) <- lift $ prepareSource s
-  go pull
-  where
-    go pull = do
-      result <- lift pull
-      case result of
-        Open x -> yield x >> go pull
-        Closed -> return ()
+sourcePipe (Source pull _) = do
+  result <- lift pull
+  case result of
+    Open s x -> yield x >> sourcePipe s
+    Closed -> return ()
 
 -- | Convert a 'Sink' into a 'Pipe'.
 --
 -- Optional consumed input is returned, together with the sink result.
 sinkPipe :: Resource m => Sink a m b -> Pipe a x (ResourceT m) (Maybe a, b)
-sinkPipe s = do
-  ps <- lift $ prepareSink s
-  case ps of
-    SinkNoData out -> return (Nothing, out)
-    SinkData push close -> go push close
+sinkPipe (SinkNoData out) = return (Nothing, out)
+sinkPipe (SinkLift m) = lift m >>= sinkPipe
+sinkPipe (SinkData p c) = go p c
   where
     go push close = do
-      x <- tryAwait
-      case x of
+      mx <- tryAwait
+      case mx of
         Nothing -> do
           out <- lift close
           return (Nothing, out)
         Just x -> do
           result <- lift $ push x
           case result of
-            Processing -> go push close
+            Processing push' close' -> go push' close'
             Done input output -> return (input, output)
 
 -- | Convert a 'Sink' into a 'Pipe', ignoring results.
 sinkPipe_ :: Resource m => Sink a m b -> Pipe a x (ResourceT m) ()
-sinkPipe_ s = sinkPipe s >> return ()
+sinkPipe_ = void . sinkPipe
